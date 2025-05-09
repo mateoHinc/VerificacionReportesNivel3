f1f0d4bb65f079111b70bd15279931efeedafb0d
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-05-09 15:26:16 -0500

    Agregar archivos de proyecto.

---
+++ VerificacionReportesNivel3/Server/Models/Cliente.cs
@@ -0,0 +1,9 @@
+﻿namespace VerificacionReportesNivel3.Server.Models
+{
+    public class Cliente
+    {
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+    }
+}
---
+++ VerificacionReportesNivel3/Server/Models/Producto.cs
@@ -0,0 +1,9 @@
+﻿namespace VerificacionReportesNivel3.Server.Models
+{
+    public class Producto
+    {
+        public string IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public int Valor { get; set; }
+    }
+}

1b7c7b17cb7aa4b482e5edf10a21e737a52ee5bd
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-05-09 15:32:36 -0500

    Añadir Clases

---
+++ VerificacionReportesNivel3/Server/Models/Factura.cs
@@ -0,0 +1,13 @@
+﻿namespace VerificacionReportesNivel3.Server.Models
+{
+    public class Factura
+    {
+        public int NoFactura { get; set; }
+        public string Cliente { get; set; }
+        public string Producto { get; set; }
+
+        // Relaciones de las tablas
+        public Cliente ClienteNav { get; set; }
+        public Producto ProductoNav { get; set; }
+    }
+}
---
+++ VerificacionReportesNivel3/Server/Models/Verificacion.cs
@@ -0,0 +1,13 @@
+﻿namespace VerificacionReportesNivel3.Server.Models
+{
+    public class Verificacion
+    {
+        public int Id { get; set; }
+        public string CedulaCliente { get; set; }
+        public bool ContieneNombre { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        //Relación de Tabla Cliente
+        public Cliente ClienteNav { get; set; }
+    }
+}

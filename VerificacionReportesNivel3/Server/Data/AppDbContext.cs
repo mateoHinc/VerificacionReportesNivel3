2ef939b32fdbf26831ff51183612f8e6801f8118
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-05-09 15:47:51 -0500

    Añadir DbContext

---
+++ VerificacionReportesNivel3/Server/Data/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VerificacionReportesNivel3.Server.Models;
+
+namespace VerificacionReportesNivel3.Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Factura> Facturas { get; set; }
+        public DbSet<Verificacion> Verificaciones { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Factura>().HasKey(f => new { f.NoFactura, f.Producto });
+
+            modelBuilder.Entity<Factura>()
+                .HasOne(f => f.ClienteNav)
+                .WithMany()
+                .HasForeignKey(f => f.Cliente)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Factura>()
+                .HasOne(f => f.ProductoNav)
+                .WithMany()
+                .HasForeignKey(f => f.Producto)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Verificacion>()
+                .HasOne(v => v.ClienteNav)
+                .WithMany()
+                .HasForeignKey(v => v.CedulaCliente)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+
+    }
+}
--- VerificacionReportesNivel3/Server/Program.cs
+++ VerificacionReportesNivel3/Server/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using VerificacionReportesNivel3.Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 

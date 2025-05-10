cac9b555cfd94da57117479b4b94e8e24293703e
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-05-10 14:10:25 -0500

    Añadir controller de clientes y hacer peticiones postman para listar todos los clientes, la cantidad de facturas a su nombre y el valor total de todas ellas

---
+++ VerificacionReportesNivel3/Server/Controllers/ClientesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VerificacionReportesNivel3.Server.Data;
+using VerificacionReportesNivel3.Server.DTOs;
+
+namespace VerificacionReportesNivel3.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ClientesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("resumen")]
+        public async Task<IActionResult> GetResumen()
+        {
+            var resumen = await _context.Clientes
+                .Select(c => new ClienteResumenDto
+                {
+                    Cedula = c.Cedula,
+                    Nombre = c.Nombre,
+                    CantidadFacturas = _context.Facturas
+                        .Where(f => f.Cliente == c.Cedula)
+                        .Select(f => f.NoFactura)
+                        .Distinct()
+                        .Count(),
+
+                    ValorTotal = _context.Facturas
+                        .Where(f => f.Cliente == c.Cedula)
+                        .Join(_context.Productos,
+                              f => f.Producto,
+                              p => p.IdProducto,
+                              (f, p) => p.Valor)
+                        .Sum()
+                }).ToListAsync();
+
+            return Ok(resumen);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ VerificacionReportesNivel3/Server/DTOs/ClienteResumenDto.cs
@@ -0,0 +1,10 @@
+﻿namespace VerificacionReportesNivel3.Server.DTOs
+{
+    public class ClienteResumenDto
+    {
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public int CantidadFacturas { get; set; }
+        public int ValorTotal { get; set; }
+    }
+}

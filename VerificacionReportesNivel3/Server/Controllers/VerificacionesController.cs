a24c9b3a256641f3e597d07caacc22d9a0c7d984
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-05-09 21:07:27 -0500

    Añadir paquete de swahbuckle.AspNetCore y crear VerificacionesController

---
+++ VerificacionReportesNivel3/Server/Controllers/VerificacionesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VerificacionReportesNivel3.Server.Data;
+using VerificacionReportesNivel3.Server.DTOs;
+using VerificacionReportesNivel3.Server.Models;
+
+namespace VerificacionReportesNivel3.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VerificacionesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public VerificacionesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/verificaciones/verificar
+        [HttpPost("verificar")]
+        public async Task<IActionResult> Verificar([FromBody] VerificacionesRequest request)
+        {
+            bool contieneNombre = request.Texto.Contains(request.Nombre ?? "", StringComparison.OrdinalIgnoreCase);
+
+            var verificacion = new Verificacion
+            {
+                CedulaCliente = request.CedulaCliente,
+                ContieneNombre = contieneNombre
+            };
+
+            _context.Verificaciones.Add(verificacion);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { contieneNombre });
+
+        }
+
+        // POST: api/verificaciones/verificar
+        [HttpGet("reporte")]
+        public IActionResult GetReporte()
+        {
+            int totalConNombre = _context.Verificaciones.Count(v => v.ContieneNombre);
+            int totalSinNombre = _context.Verificaciones.Count(v => !v.ContieneNombre);
+
+            double relacion = totalConNombre == 0 ? 0 : (double)totalConNombre / totalSinNombre;
+
+            return Ok(new
+            {
+                cuenta_contieneNombre = totalConNombre,
+                cuenta_noContieneNombre = totalSinNombre,
+                relacion
+            }
+            );
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ VerificacionReportesNivel3/Server/DTOs/VerificacionesRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace VerificacionReportesNivel3.Server.DTOs
+{
+    public class VerificacionesRequest
+    {
+        public string CedulaCliente { get; set; }
+        public string Texto { get; set; }
+        public string Nombre { get; set; }
+    }
+}
